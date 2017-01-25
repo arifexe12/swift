6f6acbd7adcfd5626a1ab4641ad7f3594ebad669
arifexe12 <arifexe12@gmail.com>
2017-01-25 21:54:33 +0600

    initial  commit

---
+++ TIMER/main.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  TIMER
+//
+//  Created by Arif Rahman on 1/24/17.
+//  Copyright © 2017 MacbookAir. All rights reserved.
+//
+
+import Foundation
+/*
+print("Hello, World!")
+var variable=0;
+let timer=Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: {_ in
+    variable += 1;
+    print("Google \(variable)");
+});
+let loop=RunLoop.main;
+loop.add(timer, forMode: RunLoopMode.defaultRunLoopMode);
+loop.run();
+while(true)
+{
+    sleep(1);
+}
+ */
+var str="Arif Rahman";
+var arr=str.components(separatedBy: " ");
+
+for a in arr{
+    print("\n Code Working : \(a)");
+}
+
