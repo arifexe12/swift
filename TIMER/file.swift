d88eb05d90851dc1480f0c2f71c152bad1122644
arifexe12 <arifexe12@gmail.com>
2017-01-29 10:03:16 +0600

    etra code

---
+++ TIMER/file.swift
@@ -0,0 +1,12 @@
+//
+//  file.swift
+//  TIMER
+//
+//  Created by Arif Rahman on 1/26/17.
+//  Copyright © 2017 MacbookAir. All rights reserved.
+//
+
+import Foundation
+
+let manager=FileManager.default;
+var path=manager.currentDirectoryPath;
